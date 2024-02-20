23cc310e2fb4b5533617f79aae68e2a77584cdf4
obidano <omotetedan2@gmail.com>
2024-02-20 15:15:54 +0100

    app config and routes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: "@react-native",
+  rules: {
+    "no-unused-vars": 1,
+    "no-inline-style": 0,
+    "prettier/prettier": ["error", { singleQuote: false }],
+    quotes: "off",
+    "comma-dangle": ["error", "never"],
+    "react-hooks/exhaustive-deps": "off",
+  },
 };
